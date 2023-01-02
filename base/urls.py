8f5d27b80c97b7310c22dcf79d72afb40ec496c8
Filip Tatarnik <Ftatarnik@gmail.com>
2023-01-02 14:39:27 -0600

    complete CRUD functionality +user registration +login + unique home pages for unique logins. adding styling now

--- base/urls.py
+++ base/urls.py
@@ -5,10 +5,12 @@ from .views import TaskCreate
 from .views import TaskUpdate
 from .views import DeleteView
 from .views import CustomLoginView
+from .views import RegisterPage
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('login/', CustomLoginView.as_view(), name='login'),
+    path('register/', RegisterPage.as_view(), name='register'),
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
     path('', TaskList.as_view(), name='tasks'),
     path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
